fb23f1c792185acd255759bbd47f0596e935f8a6
Aleksey Kuptsov <liberate.tutemae1@gmail.com>
2022-02-25 15:22:21 +0700

    start

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 500,
+        prevArrow: '<button type="button" class ="slick-prev"><img src="../icons/arrows/left.svg"></img></button>',
+        nextArrow: '<button type="button" class ="slick-next"><img src="../icons/arrows/right.svg"></img></button>'
+    });
+});
